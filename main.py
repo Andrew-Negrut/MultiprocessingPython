54d1dbdb2bf02f1463f061e87f0c3a4a98851b71
Andrew-Negrut <45469110+Andrew-Negrut@users.noreply.github.com>
2023-07-03 23:16:44 -0500

    finished the tutorial
    new concurrent.futures technique

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 # based off the tutorial by Corey Schafer
 
 import multiprocessing
+import concurrent.futures
 import time
 
 def do_something(seconds):
     print(f'Sleeping {seconds} seconds...')
     time.sleep(seconds)
-    print('Done sleeping...')
+    return f'Done sleeping...{seconds}'
 
 if __name__ == "__main__":
     start = time.perf_counter()
 
-    processes = []
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        seconds = [5, 4, 3, 2, 1]
+        results = executor.map(do_something, seconds)
+        # results = [executor.submit(do_something, sec) for sec in seconds]
 
-    for _ in range(10):
-        p = multiprocessing.Process(target=do_something, args=[1.5])
-        p.start()
-        processes.append(p)
+        for result in results:
+            print(result)
+
+        # for f in concurrent.futures.as_completed(results):
+        #     print(f.result())
+
+    # processes = []
+
+    # for _ in range(10):
+    #     p = multiprocessing.Process(target=do_something, args=[1.5])
+    #     p.start()
+    #     processes.append(p)
     
-    for process in processes:
-        process.join()
+    # for process in processes:
+    #     process.join()
 
     finish = time.perf_counter()
 
